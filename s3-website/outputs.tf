a7f291b96ce132220329ac1657f4d4022c92cf5e
dhanbdrkarki1 <dhanbdrkarki111@gmail.com>
2024-01-21 21:13:00 +0545

    subdomain added.

--- s3-website/outputs.tf
+++ s3-website/outputs.tf
@@ -2,6 +2,10 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.cf.domain_name
 }
 
-output "domain_name" {
+output "root_domain" {
+  value = aws_route53_record.root.name
+}
+
+output "website_link" {
   value = aws_route53_record.www.name
 }
